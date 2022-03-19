c948c70457223847ac1fa8f697345d1dbda119eb
HM <hieumdd@gmail.com>
2022-03-19 23:55:26 +0700

    misc: Rename service

--- src/ui.ts
+++ src/ui.ts
@@ -2,11 +2,11 @@ const ui = SpreadsheetApp.getUi();
 
 const onOpen = () => {
     ui.createMenu('VNBI - NetSuite')
-        .addItem('Show Side Bar', 'showSideBar')
+        .addItem('Import CSV', 'importCSVService')
         .addToUi();
 };
 
-const showSideBar = () => {
+const importCSVService = () => {
     const html = HtmlService.createTemplateFromFile('form')
         .evaluate()
         .setTitle('Import CSV');
